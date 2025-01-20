6fa476606c7b4d5a8b59fb1ce8bb175c1f3c40ba
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-01-20 11:11:29 -0300

    Implement window selection

--- x11.c
+++ x11.c
@@ -1,3 +1,4 @@
+#include "cli_io.h"
 #include "window_manager.h"
 
 // Select window with the cursor and return its name
@@ -6,6 +7,28 @@ bool select_window(string *name) {
   getchar();
   printf("Click on the app...\n");
 
-  // TODO
-  return false;
+  // Get PID
+  string pid = STR_INIT;
+  int ret = console("xdotool selectwindow getwindowpid", &pid);
+  if (ret) {
+    str_free(&pid);
+    ERROR("Error while getting the PID of the window. The app is not compatible...");
+    return false;
+  }
+
+  // Get name
+  string cmd = STR_INIT;
+  str_append(&cmd, "ps -p ");
+  str_append(&cmd, pid.str);
+  str_append(&cmd, " -o comm=");
+  ret = console(cmd.str, name);
+  str_free(&pid);
+  str_free(&cmd);
+  if (ret) {
+    ERROR("Error while getting the name of the window.");
+    str_free(name);
+    return false;
+  }
+
+  return true;
 }
